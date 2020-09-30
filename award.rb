94f4b6719fc3eff0e539e697f07793bea8f66838
jandersen770 <jandersen770@gmail.com>
2020-09-30 07:50:37 -0400

    Split and join varible name to match class name, remove hash

--- award.rb
+++ award.rb
@@ -1,20 +1,12 @@
-CLASS = {
-    'NORMAL ITEM' => 'NormalItem',
-    'Blue First' => 'BlueFirst',
-    'Blue Distinction Plus' => 'BlueDistinctionPlus',
-    'Blue Compare' => 'BlueCompare',
-    'Blue Star' => 'BlueStar'
-}
-
 Award = Struct.new(:name, :expires_in, :quality) do
 
     def self.new(name, expires_in, quality)
-        Object.const_get(CLASS[name]).new(name, expires_in, quality)
+        Object.const_get(name.split(" ").join("")).new(name, expires_in, quality)
     end
 
 end
 
-NormalItem = Struct.new(:name, :expires_in, :quality) do
+NORMALITEM = Struct.new(:name, :expires_in, :quality) do
 
     def limit
         self.quality > 50 ? 50 : self.quality <= 0 ? 0 : self.quality 
@@ -32,7 +24,7 @@ NormalItem = Struct.new(:name, :expires_in, :quality) do
 
 end
 
-class BlueFirst < NormalItem
+class BlueFirst < NORMALITEM
     # increase in quality the older they get
     def appreciate
         self.expires_in > 0 ? self.quality += 1 : self.quality += 2
@@ -42,7 +34,7 @@ class BlueFirst < NormalItem
 
 end
 
-class BlueDistinctionPlus < NormalItem
+class BlueDistinctionPlus < NORMALITEM
     # never decreases in quality / being highly sought, its quality is 80 and it never alters.
     def appreciate
         self.quality = 80
@@ -54,7 +46,7 @@ class BlueDistinctionPlus < NormalItem
 
 end
 
-class BlueCompare < NormalItem
+class BlueCompare < NORMALITEM
     # Quality increases by 2 when there are 10 days or less left, and by 3 where there are 5 days or less left, but quality value drops to 0 after the expiration date.
     def appreciate
         if self.expires_in <= 0 
@@ -72,7 +64,7 @@ class BlueCompare < NormalItem
 
 end
 
-class BlueStar < NormalItem
+class BlueStar < NORMALITEM
     # lose quality value twice as fast as normal awards
     def appreciate
         self.expires_in > 0 ? self.quality -= 2 : self.quality -= 4
