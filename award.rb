940a07d3e8a752428215048963c4824820a1d939
jandersen770 <jandersen770@gmail.com>
2020-09-29 22:46:13 -0400

    Use Award as a class builder so rspec wont solely use Award, use names to key into hash to contruct varied classes, change BlueCompare.appreciate to if else conditional for readability, include self.limit corrections in Blue First, Compare & Star, add age method returning nil in glorious BlueDistinctionPlus, and return 0 if quality equals 0 in now NormalItem's (previously denoted as Award) appreciate method

--- award.rb
+++ award.rb
@@ -1,11 +1,29 @@
+CLASS = {
+    'NORMAL ITEM' => 'NormalItem',
+    'Blue First' => 'BlueFirst',
+    'Blue Distinction Plus' => 'BlueDistinctionPlus',
+    'Blue Compare' => 'BlueCompare',
+    'Blue Star' => 'BlueStar'
+}
+
 Award = Struct.new(:name, :expires_in, :quality) do
 
-    # def limit
-    #     self.quality > 50 ? 50 : self.quality < 0 ? 0 : self.quality 
-    # end
+    def self.new(name, expires_in, quality)
+        Object.const_get(CLASS[name]).new(name, expires_in, quality)
+    end
+
+end
+
+NormalItem = Struct.new(:name, :expires_in, :quality) do
+
+    def limit
+        self.quality > 50 ? 50 : self.quality <= 0 ? 0 : self.quality 
+    end
 
     def appreciate
-        self.quality -= 1
+        return 0 if self.quality <= 0
+        
+        self.expires_in > 0 ? self.quality -= 1 : self.quality -= 2
     end
 
     def age
@@ -14,34 +32,52 @@ Award = Struct.new(:name, :expires_in, :quality) do
 
 end
 
-class BlueFirst < Award
+class BlueFirst < NormalItem
     # increase in quality the older they get
     def appreciate
-        self.expires_in > 0 ? self.quality += 1 : self.quality += expires_in.abs() if self.quality < 50
+        self.expires_in > 0 ? self.quality += 1 : self.quality += 2
+
+        self.quality = self.limit
     end
 
 end
 
-class BlueDistinctionPlus < Award
+class BlueDistinctionPlus < NormalItem
     # never decreases in quality / being highly sought, its quality is 80 and it never alters.
     def appreciate
         self.quality = 80
     end
 
-end
-
-class BlueCompare < Award
-    # Quality increases by 2 when there are 10 days or less left, and by 3 where there are 5 days or less left, but quality value drops to 0 after the expiration date.
-    def appreciate
-        self.expires_in <= 10 ? self.quality += 2 : self.expires_in <= 5 ? self.quality += 3 : self.expires_in <= 0 ? self.quality = 0 : self.quality += 1
+    def age
+        nil
     end
 
 end
 
-class BlueStar < Award
+class BlueCompare < NormalItem
+    # Quality increases by 2 when there are 10 days or less left, and by 3 where there are 5 days or less left, but quality value drops to 0 after the expiration date.
+    def appreciate
+        if self.expires_in <= 0 
+            self.quality = 0 
+        elsif self.expires_in <= 10 && self.expires_in >= 6 
+            self.quality += 2 
+        elsif self.expires_in <= 5 
+            self.quality += 3
+        else
+            self.quality += 1 
+        end
+
+        self.quality = self.limit
+    end
+
+end
+
+class BlueStar < NormalItem
     # lose quality value twice as fast as normal awards
     def appreciate
-        self.expires_in > 0 ? self.quality -= 2 : self.quality -= ( self.expires_in.abs() * 2 )
+        self.expires_in > 0 ? self.quality -= 2 : self.quality -= 4
+
+        self.quality = self.limit
     end
 
 end
